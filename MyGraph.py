bbd4da7ca3f2d8c366f159c537e69145ccf5e253
Andrew Arthur <andrew_arthur1@baylor.edu>
2024-01-24 13:29:01 -0600

    updated gitignor and changed print statement

--- MyGraph.py
+++ MyGraph.py
@@ -6,4 +6,12 @@ plt.plot(x, np.sin(x))
 
 plt.show()
 
-print("Hi there!")
+print("Hello World!")
+
+# Installing a Library:
+# 1) Create a virtual environment (in terminal):
+#       py -3 -m venv ~environmentName~
+# 2) Activate your VE
+#       ~environmentName~\Scripts\activate
+# 3) Install 3p library
+#       pip3 install ~libraryName~
